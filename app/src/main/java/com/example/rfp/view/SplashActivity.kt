d4016458f8415b8cc50feaeddc90ec130ea0eded
정빈 <43386290+B1ns@users.noreply.github.com>
2021-09-03 02:28:55 +0900

    [Update] SDR-Team Start!

---
+++ app/src/main/java/com/example/rfp/view/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.rfp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.rfp.MainActivity
+import com.example.rfp.R
+import org.jetbrains.anko.startActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity<MainActivity>()
+        }, 3000)
+
+    }
+}
