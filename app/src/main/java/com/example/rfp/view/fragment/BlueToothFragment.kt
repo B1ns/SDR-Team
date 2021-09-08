2428d70bfb3aa8992f6a523a490a250a13e1dacf
정빈 <43386290+B1ns@users.noreply.github.com>
2021-09-09 01:32:11 +0900

    [Update] Bluetooth Scan & Main Fragment UI

--- app/src/main/java/com/example/rfp/view/fragment/BlueToothFragment.kt
+++ app/src/main/java/com/example/rfp/view/fragment/BlueToothFragment.kt
@@ -1,6 +1,7 @@
 package com.example.rfp.view.fragment
 
 import android.animation.ValueAnimator
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,40 +9,31 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.rfp.R
 import com.example.rfp.databinding.FragmentBlueToothBinding
-import android.widget.ArrayAdapter
 
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothAdapter
-import android.content.Context
 import android.content.Intent
-
-import android.content.IntentFilter
-import android.content.BroadcastReceiver
 import org.jetbrains.anko.support.v4.toast
-import com.example.rfp.data.ConnectedThread
-
-import android.widget.Toast
 
 import android.widget.AdapterView
-import android.widget.AdapterView.OnItemClickListener
-import java.io.IOException
+import android.util.Log
+import android.widget.ArrayAdapter
+import kotlin.collections.ArrayList
 
-import android.bluetooth.BluetoothSocket
 
 class BlueToothFragment : Fragment() {
 
-    private lateinit var pairedDevices: Set<BluetoothDevice>
-    private lateinit var btArrayAdapter: ArrayAdapter<String>
-    private lateinit var deviceAddressArray: ArrayList<String>
+    private var m_bluetoothAdapter: BluetoothAdapter? = null
+    private lateinit var m_pairedDevices: Set<BluetoothDevice>
+    private val REQUEST_ENABLE_BLUETOOTH = 1
 
-    private lateinit var btAdapter: BluetoothAdapter
-    private val REQUEST_ENABLE_BT = 1
+    companion object {
+        val EXTRA_ADDRESS: String = "Device_address"
+    }
+
 
     private var _binding: FragmentBlueToothBinding? = null
     private val binding get() = _binding!!
-
-    private lateinit var btSocket: BluetoothSocket
-    private lateinit var connectedThread: ConnectedThread
 
 
     override fun onCreateView(
@@ -50,120 +42,83 @@ class BlueToothFragment : Fragment() {
     ): View {
         _binding = FragmentBlueToothBinding.inflate(inflater, container, false)
 
-        btAdapter = BluetoothAdapter.getDefaultAdapter()
-        if (!btAdapter.isEnabled) {
-            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (m_bluetoothAdapter == null) {
+            toast("This device dosen't support bluetooth")
+        }
+        if (!m_bluetoothAdapter!!.isEnabled) {
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
         }
 
-        btArrayAdapter = ArrayAdapter(activity?.applicationContext!!, R.layout.bluetooth_list_item)
-        deviceAddressArray = ArrayList()
-        binding.bluetoothList.adapter = btArrayAdapter
 
-//        binding.bluetoothList.onItemClickListener = myOnItemClickListener()
-
-        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val send = { _: View ->
-
-        }
 
         val search = { _: View ->
             toast("Search")
             search()
         }
 
-        val paired = { _: View ->
-            toast("Paired Device")
-            paired()
-        }
-        binding.sendBtn.setOnClickListener(send)
         binding.searchBtn.setOnClickListener(search)
-        binding.pairedBtn.setOnClickListener(paired)
 
     }
 
-    private fun paired(): Unit {
-        btArrayAdapter.clear()
-        if (deviceAddressArray.isNotEmpty()) {
-            deviceAddressArray.clear()
-        }
-        pairedDevices = btAdapter.bondedDevices
-        if (pairedDevices.isNotEmpty()) {
-            for (device in pairedDevices) {
-                val deviceName = device.name
-                val deviceHardwareAddress = device.address
-                btArrayAdapter.add(deviceName)
-                deviceAddressArray.add(deviceHardwareAddress)
+
+    private fun search() {
+
+
+        val deviceList: ArrayList<BluetoothDevice> = ArrayList()
+        val nameList: ArrayList<String> = ArrayList()
+
+        if (m_pairedDevices.isNotEmpty()) {
+            for (device: BluetoothDevice in m_pairedDevices) {
+                deviceList.add(device)
+                nameList.add(device.name + "(" + device.address + ")")
+                Log.i("device", "" + device.name)
             }
-        }
-    }
-
-    private fun search(): Unit {
-        binding.bluetoothLottie.repeatCount = ValueAnimator.INFINITE
-
-        if (btAdapter.isDiscovering) {
-            btAdapter.cancelDiscovery()
         } else {
-            if (btAdapter.isEnabled) {
-                btAdapter.startDiscovery()
-                btArrayAdapter.clear()
-                if (deviceAddressArray.isNotEmpty()) {
-                    deviceAddressArray.clear()
+            toast("페어링된 장치를 찾을 수 없음")
+        }
+
+        val listAdapter = ArrayAdapter<String>(requireContext(), R.layout.bluetooth_list_item, R.id.bluetooth_text_item)
+        binding.bluetoothList.adapter = listAdapter
+
+        binding.bluetoothList.onItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+                val device: BluetoothDevice = deviceList[position]
+                val address: String = device.address
+
+                val intent = Intent(requireActivity().applicationContext, MainFragment::class.java)
+                intent.putExtra(EXTRA_ADDRESS, address)
+                startActivity(intent)
+            }
+    }
+
+    override fun onActivityResult(requestCode:Int, resultCode:Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_ENABLE_BLUETOOTH) {
+            if(resultCode == Activity.RESULT_OK) {
+                if(m_bluetoothAdapter!!.isEnabled) {
+                    toast("Bluetooth 연결성공 !")
                 }
-                val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-                requireActivity().registerReceiver(receiver, filter)
-            } else {
+                else {
+                    toast("Bluetooth 연결실패..")
+                }
             }
+            else if(resultCode == Activity.RESULT_CANCELED) {
+                toast("Bluetooth 활성화가 취소되었습니다.")
+            }
+
         }
     }
-
-    private val receiver: BroadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            val action = intent.action
-            if (BluetoothDevice.ACTION_FOUND == action) {
-                val device =
-                    intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
-                val deviceName = device!!.name
-                val deviceHardwareAddress = device.address
-                btArrayAdapter.add(deviceName)
-                deviceAddressArray.add(deviceHardwareAddress)
-                btArrayAdapter.notifyDataSetChanged()
-            }
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        activity?.unregisterReceiver(receiver)
-    }
-
-//    inner class myOnItemClickListener : OnItemClickListener {
-//        override fun onItemClick(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
-//            textStatus.setText("try...")
-//            val name: String = btArrayAdapter.getItem(position).toString() // get name
-//            val address: String = deviceAddressArray[position] // get address
-//            var flag = true
-//            val device: BluetoothDevice = btAdapter.getRemoteDevice(address)
-//
-//            // create & connect socket
-//            try {
-//                btSocket = createBluetoothSocket(device)
-//                btSocket.connect()
-//            } catch (e: IOException) {
-//                flag = false
-//                textStatus.setText("connection failed!")
-//                e.printStackTrace()
-//            }
-//            if (flag) {
-//                textStatus.setText("connected to $name")
-//                connectedThread = ConnectedThread(btSocket)
-//                connectedThread.start()
-//            }
-//        }
-//    }
 }
